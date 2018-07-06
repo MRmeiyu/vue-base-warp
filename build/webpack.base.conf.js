9f5cfa0786f926c951fce352eef358e8f3ee3954
MeiYU <mieyu@10.qq.com>
2018-07-06 16:28:22 +0800

    增加less

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -33,7 +33,7 @@ module.exports = {
       : config.dev.assetsPublicPath
   },
   resolve: {
-    extensions: ['.js', '.vue', '.json', '.sass'],
+    extensions: ['.js', '.vue', '.json', '.less'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
